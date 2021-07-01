d7c72a0220939b5e3002f2da12e13c17c3825386
Lbacoc2 <85352099+Lbacoc2@users.noreply.github.com>
2021-06-30 22:15:08 -0500

    actualice información

--- src/componets/cards/ListCards.js
+++ src/componets/cards/ListCards.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import Cards from './Cards'
+//import celebridades from '../../helpers/usuarios'
+import React, { useEffect, useState} from 'react'
 
 const ListCards= () => {
+    const [celebridades, setcelebridades] = useState([])
+    const [error, setError] = useState(false)
+    useEffect(() => {
+       const getusuarios = async() => {
+           try{
+                const res = await fetch("https://randomuser.me/api/?results=70")
+                const data = res.json()
+                setcelebridades(data.results)
+                setError(false)
+            } catch(err) {
+           console.log(err)
+           setError(true)
+       }
+       getusuarios()
+    }, [])
+    if(error){
+        return 
+    }
+    <div></div>
+    // console.log(celebridades[0].name.last)
     return (
-        <div>
-            <h1>Listado de Cards</h1>
-            <Cards/>
+        <div className="container">
+            <div className="row">
+            { celebridades.map(users => (
+                <div className="col-sm-6 col-nd-4 col-lg-3" key={users.login.uuid}>
+                    <Cards user={user}/>
+                </div>
+            ))
+            }
+            </div>
         </div>
     )
 }
